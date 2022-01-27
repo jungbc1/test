97cea45a2c7a2b2610524c3659e9bd34a35306d7
jungbc1 <tonke1234@naver.com>
2022-01-27 23:55:18 +0900

    onclick

--- js/payment.js
+++
@@ -1,4 +0,0 @@
-// onclick달린 a태그의 이동막기.
-function preventClick(e){
-	e.preventDefault()
-}
---
+++ js/shop_payment.js
@@ -0,0 +1,15 @@
+const select_pay = function(tag){
+	console.log(tag);
+	// let periods = document.getElementsByClassName("on")
+    // for (const period of periods) {
+    //     period.classList.remove("active");
+    // }
+    // tag.classList.add("active");
+	// preventClick(event);
+}
+
+// onclick달린 a태그의 이동막기.
+function preventClick(e){
+	e.preventDefault()
+	// log(e.target)
+}
